7b20d4993dc83d628db6290d9983bb6dc14a4fb2
Juanes07 <juanesdm3@gmail.com>
2022-06-24 18:14:22 -0500

    prueba back proyecto base

---
+++ src/main/java/com/sofka/gestionRiesgo/GestionRiesgoApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.gestionRiesgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRiesgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRiesgoApplication.class, args);
+    }
+
+}
