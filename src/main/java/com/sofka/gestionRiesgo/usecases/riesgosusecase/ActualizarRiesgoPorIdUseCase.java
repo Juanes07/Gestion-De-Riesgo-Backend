4f276ea51fd8c69dffcc1f618eebd06251e9afb1
Camila Morales <camila.moralesg21@gmail.com>
2022-06-25 17:16:42 -0500

    ADD: actualizar riesgo usecase
    Se crea el caso de uso y el router para actualizar un riesgo

--- src/main/java/com/sofka/gestionRiesgo/usecases/riesgosusecase/ActualizarRiesgoPorIdUseCase.java
+++ src/main/java/com/sofka/gestionRiesgo/usecases/riesgosusecase/ActualizarRiesgoPorIdUseCase.java
@@ -3,21 +3,31 @@ package com.sofka.gestionRiesgo.usecases.riesgosusecase;
 import com.sofka.gestionRiesgo.mappers.MapperRiesgo;
 import com.sofka.gestionRiesgo.models.RiesgoDTO;
 import com.sofka.gestionRiesgo.repository.GuardarRiesgo;
+import com.sofka.gestionRiesgo.repository.RiesgoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
 
-//public class ActualizarRiesgoPorIdUseCase implements GuardarRiesgo {
-//    private final GuardarRiesgo guardarRiesgo;
-//    private final MapperRiesgo mapperRiesgo;
-//
-//    public ActualizarRiesgoPorIdUseCase(GuardarRiesgo guardarRiesgo, MapperRiesgo mapperRiesgo) {
-//        this.guardarRiesgo = guardarRiesgo;
-//        this.mapperRiesgo = mapperRiesgo;
-//    }
-//
-//
-////    @Override
-////    public Mono<String> apply(RiesgoDTO riesgoDTO) {
-////        return null;
-////    }
-////}
-////
+import java.util.Objects;
+
+@Service
+@Validated
+public class ActualizarRiesgoPorIdUseCase implements GuardarRiesgo {
+    private final RiesgoRepository riesgoRepository;
+    private final MapperRiesgo mapperRiesgo;
+
+    public ActualizarRiesgoPorIdUseCase(RiesgoRepository riesgoRepository, MapperRiesgo mapperRiesgo) {
+        this.riesgoRepository = riesgoRepository;
+        this.mapperRiesgo = mapperRiesgo;
+    }
+
+    @Override
+    public Mono<RiesgoDTO> apply(RiesgoDTO riesgoDTO) {
+        Objects.requireNonNull(riesgoDTO.getId(), "El id del proyecto es requerido");
+        return riesgoRepository
+                .save(mapperRiesgo.riesgoDtoARiesgo(riesgoDTO.getId())
+                        .apply(riesgoDTO))
+                .thenReturn(riesgoDTO);
+    }
+}
+
