267b1fbcc179c0ff4ce8a06470c3841817890305
duvis07 <duvanarleybotero@gmail.com>
2022-06-27 09:38:05 -0500

    se implementa swagger
    se implementa swagger para la documentacion del  backend

--- src/main/java/com/sofka/gestionRiesgo/routers/UsuarioRouter.java
+++ src/main/java/com/sofka/gestionRiesgo/routers/UsuarioRouter.java
@@ -1,9 +1,18 @@
 package com.sofka.gestionRiesgo.routers;
 
+import com.sofka.gestionRiesgo.models.RiesgoDTO;
 import com.sofka.gestionRiesgo.models.UsuarioDTO;
+import com.sofka.gestionRiesgo.usecases.riesgosusecase.CrearRiesgoUseCase;
 import com.sofka.gestionRiesgo.usecases.usuariosusecase.ActualizarUsuarioPorIdUseCase;
 import com.sofka.gestionRiesgo.usecases.usuariosusecase.CrearUsuarioUseCase;
 import com.sofka.gestionRiesgo.usecases.usuariosusecase.ObtenerUsuariosUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -23,6 +32,16 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class UsuarioRouter {
 
     @Bean
+    @RouterOperation(beanClass = CrearUsuarioUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "usuarioDTO", summary = "Crear Usuario", tags = {"Usuario"},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer"),
+                            @Parameter(in = ParameterIn.PATH, name = "nombre", description = "String"),
+                            @Parameter(in = ParameterIn.PATH, name = "email", description = "String"),
+                            @Parameter(in = ParameterIn.PATH, name = "roles", description = "List<String>")},
+
+                    responses = {@ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = UsuarioDTO.class))),
+                            @ApiResponse(responseCode = "400", description = "Invalid user supplied"),
+                            @ApiResponse(responseCode = "404", description = "User not found")}))
     public RouterFunction<ServerResponse> crearUsuario(CrearUsuarioUseCase useCase) {
         Function<UsuarioDTO, Mono<ServerResponse>> executor = usuarioDTO -> useCase.apply(usuarioDTO)
                 .flatMap(result -> ServerResponse.ok()
@@ -36,6 +55,16 @@ public class UsuarioRouter {
     }
 
     @Bean
+    @RouterOperation(beanClass = CrearUsuarioUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "usuarioDTO", summary = "Actualizar usuario por id", tags = {"Usuario"},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer"),
+                            @Parameter(in = ParameterIn.PATH, name = "nombre", description = "String"),
+                            @Parameter(in = ParameterIn.PATH, name = "email", description = "String"),
+                            @Parameter(in = ParameterIn.PATH, name = "roles", description = "List<String>")},
+
+                    responses = {@ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = UsuarioDTO.class))),
+                            @ApiResponse(responseCode = "400", description = "Invalid user supplied"),
+                            @ApiResponse(responseCode = "404", description = "User not found")}))
     public RouterFunction<ServerResponse> actualizarUsuario(ActualizarUsuarioPorIdUseCase useCase) {
         Function<UsuarioDTO, Mono<ServerResponse>> executor = usuarioDTO -> useCase.apply(usuarioDTO)
                 .flatMap(result -> ServerResponse.ok()
@@ -49,6 +78,10 @@ public class UsuarioRouter {
     }
 
     @Bean
+    @RouterOperation(operation = @Operation(operationId = "Obtener todas los usuarios", summary = "Obtener todas los usuarios", tags = {"Usuario"},
+            responses = {@ApiResponse(responseCode = "200", description = "successful operation"),
+                    @ApiResponse(responseCode = "400", description = "Invalid user supplied"),
+                    @ApiResponse(responseCode = "404", description = "User not found")}))
     public RouterFunction<ServerResponse> obtenerUsuarios(ObtenerUsuariosUseCase useCase) {
         return route(GET("/obtenerUsuarios").and(accept(MediaType.APPLICATION_JSON)),
                 request -> ServerResponse.ok()
--- src/test/java/com/sofka/gestionRiesgo/usecases/proyectousecase/ObtenerProyectoPorIdUseCaseTest.java
+++ src/test/java/com/sofka/gestionRiesgo/usecases/proyectousecase/ObtenerProyectoPorIdUseCaseTest.java
@@ -57,5 +57,6 @@ class ObtenerProyectoPorIdUseCaseTest {
 
         verify(proyectoRepository).findById(proyectoDto.getId());
 
+
     }
 }
